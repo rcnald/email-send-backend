8f292ff01e947a1716be7ae8becc3600ecf17d35
Ronaldo Junior <ronaldomjunior05@gmail.com>
2026-02-23 22:28:12 -0300

    fix: allow cookies to be setted on localhost

--- src/infra/http/controllers/auth/authenticate.ts
+++ src/infra/http/controllers/auth/authenticate.ts
@@ -41,14 +41,14 @@ export class AuthenticateController {
     response.cookie("accessToken", result.accessToken, {
       httpOnly: true,
       secure: this.env.ENVIRONMENT === "production",
-      sameSite: "strict",
+      sameSite: this.env.ENVIRONMENT === "production" ? "strict" : "none",
       maxAge: this.env.JWT_ACCESS_TOKEN_MAX_AGE,
     });
 
     response.cookie("refreshToken", result.refreshToken, {
       httpOnly: true,
       secure: this.env.ENVIRONMENT === "production",
-      sameSite: "strict",
+      sameSite: this.env.ENVIRONMENT === "production" ? "strict" : "none",
       maxAge: this.env.JWT_REFRESH_TOKEN_MAX_AGE,
     });
 
